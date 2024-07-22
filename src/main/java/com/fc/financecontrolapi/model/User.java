5935e82c95befd8ebdbeb836cb8d75c7c66ffba9
Luiz Reis <reisluiz05@gmail.com>
2024-07-22 20:07:02 -0300

    changing user-role fetch type to lazy

--- src/main/java/com/fc/financecontrolapi/model/User.java
+++ src/main/java/com/fc/financecontrolapi/model/User.java
@@ -27,7 +27,7 @@ public class User implements UserDetails {
     private String password;
     private Instant createdAt;
     private Instant deletedAt;
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "role_id")
     private Role role;
 
