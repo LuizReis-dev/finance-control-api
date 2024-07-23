7992115d25a6801e5a8d26e947ec9036b7e99175
Luiz Reis <reisluiz05@gmail.com>
2024-07-22 23:13:35 -0300

    creating categories table and category entity

---
+++ src/main/java/com/fc/financecontrolapi/model/Category.java
@@ -0,0 +1,24 @@
+package com.fc.financecontrolapi.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_categories")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean isActive;
+    private Instant createdAt;
+    private Instant deleteAt;
+}
